c6e64dfca2394991d19b0315b6f9a42503ee7d99
ro6drigo <ro6drigo@hotmail.com>
2016-05-23 23:41:11 -0500

    Estructura Inicial

---
+++ Wanlla/Wanlla/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Wanlla.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/Scripts/jquery-2.2.3.js", //JQuery 2.2.3
+                        "~/Scripts/bootstrap.min.js")); //JS Bootstrap 3.3.6
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css", //CSS Bootstrap 3.3.6
+                      "~/Content/font-awesome.min.css", //Font Awesome
+                      "~/Content/own-style.css", //Own Styles (POR MINIMIZAR)
+                      "~/Content/sticky-footer.css")); //Sticky Footer (POR MINIMIZAR)
+        }
+    }
+}
---
+++ Wanlla/Wanlla/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wanlla.MvcApplication" Language="C#" %>
