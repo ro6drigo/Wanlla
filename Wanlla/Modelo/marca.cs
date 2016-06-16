2da7f2090e679da8be4c0f11d72c9b049d572656
FernandoJ27 <fjjc27@gmail.com>
2016-06-16 13:54:10 -0500

    Mantenimiento Completo - Marca

--- Wanlla/Modelo/marca.cs
+++ Wanlla/Modelo/marca.cs
@@ -4,7 +4,9 @@ namespace Modelo
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("marca")]
     public partial class marca
@@ -24,5 +26,105 @@ namespace Modelo
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<producto> producto { get; set; }
+
+        public List<marca> Listar()
+        {
+            var marcas = new List<marca>();
+
+            try
+            {
+                using (var dbwanlla = new db_wanlla())
+                {
+                    marcas = dbwanlla.marca.ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return marcas;
+        }
+
+        public List<marca> Buscar(string criterio)
+        {
+            var marcas = new List<marca>();
+            
+            try
+            {
+                using (var dbwanlla = new db_wanlla())
+                {
+                    marcas = dbwanlla.marca
+                                .Where(x => x.nom_marca.Contains(criterio))
+                                .ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return marcas;
+        }
+
+        public marca Obtener(int id)
+        {
+            var marca = new marca();
+
+            try
+            {
+                using (var dbwanlla = new db_wanlla())
+                {
+                    marca = dbwanlla.marca
+                        .Where(x => x.id_marca == id)
+                        .SingleOrDefault();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return marca;
+        }
+
+        public void Guardar()
+        {
+            try
+            {
+                using (var dbwanlla = new db_wanlla())
+                {
+                    if (this.id_marca > 0)
+                    {
+                        dbwanlla.Entry(this).State = EntityState.Modified;
+                    }
+                    else
+                    {
+                        dbwanlla.Entry(this).State = EntityState.Added;
+                    }
+                    dbwanlla.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Eliminar()
+        {
+            try
+            {
+                using (var dbwanlla = new db_wanlla())
+                {
+                    dbwanlla.Entry(this).State = EntityState.Deleted;
+                    dbwanlla.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
--- Wanlla/Wanlla/Areas/Admin/Controllers/MarcaController.cs
+++ Wanlla/Wanlla/Areas/Admin/Controllers/MarcaController.cs
@@ -3,15 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Modelo;
+using Wanlla.Filters;
 
 namespace Wanlla.Areas.Admin.Controllers
 {
+    [Autenticado]
     public class MarcaController : Controller
     {
-        // GET: Admin/Marca
-        public ActionResult Index()
+        // GET: Marca
+        private marca marca = new marca();
+
+        public ActionResult Index(string criterio)
         {
-            return View();
+            if (criterio == null | criterio == "")
+            {
+                return View(marca.Listar());
+            }
+            else
+            {
+                return View(marca.Buscar(criterio));
+            }
         }
+
+        public ActionResult Mantenimiento(int id = 0)
+        {
+            return View(
+                id == 0 ? new marca() //nueva categoria
+                        : marca.Obtener(id)
+            );
+        }
+
+        public ActionResult Guardar(marca model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Guardar();
+                return Redirect("~/Admin/Marca/");
+            }
+            else
+            {
+                return View("~/Admin/Marca/Mantenimiento.cshtml", model);
+            }
+
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            marca.id_marca = id;
+            marca.Eliminar();
+            return Redirect("~/Admin/Marca/");
+        }
+
     }
 }
