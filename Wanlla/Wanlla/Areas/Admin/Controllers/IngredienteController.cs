6ae64819aadfd6b7c358948c1bf3ddc6077c77f9
GueSosLim <guesoslim@gmail.com>
2016-06-17 12:12:59 -0500

    ingrediente controller

--- Wanlla/Wanlla/Areas/Admin/Controllers/IngredienteController.cs
+++ Wanlla/Wanlla/Areas/Admin/Controllers/IngredienteController.cs
@@ -16,19 +16,62 @@ namespace Wanlla.Controllers
         /// </summary>
         private ingrediente ingrediente = new ingrediente();
         // GET: Ingrediente
-        public ActionResult Index()
+        public ActionResult Index(string nom_ingrediente, string tipo_ingrediente)
         {
-            return View();
+            if (nom_ingrediente == null || nom_ingrediente == "")
+            {
+                return View(ingrediente.listar());
+            }
+            else
+            {
+                return View(ingrediente.buscar(nom_ingrediente, tipo_ingrediente));
+            }
         }
 
-        public ActionResult Detalle()
+        public JsonResult CargarGrilla(AnexGRID grid)
         {
-            return View();
+            return Json(ingrediente.ListarGrilla(grid));
         }
 
-        public ActionResult Mantenimiento()
+        public ActionResult Detalle(int id)
         {
-            return View();
+            return View(ingrediente.Obtener(id));
+        }
+
+        public ActionResult Mantenimiento(int id = 0)
+        {
+            return View(
+                id == 0 ? new ingrediente()
+                        : ingrediente.Obtener(id)
+                );
+        }
+
+        public ActionResult Guardar(ingrediente model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.mantenimiento();
+                return Redirect("~/Admin/Ingrediente/Index");
+            }
+            else
+            {
+                return View("~/Admin/Views/Ingrediente/Mantenimiento.cshtml", model);
+            }
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            ingrediente.id_ingrediente = id;
+            ingrediente.eliminar();
+            return Redirect("~/Admin/Ingrediente/Index");
+        }
+
+        public ActionResult Buscar(string nom_ingrediente, string tipo_ingrediente)
+        {
+            return View(
+                    nom_ingrediente == null || nom_ingrediente == "" ? ingrediente.listar()
+                    : ingrediente.buscar(nom_ingrediente, tipo_ingrediente)
+                    );
         }
     }
 }
