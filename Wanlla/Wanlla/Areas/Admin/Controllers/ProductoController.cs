50025b446b1c97e91c24d0daacbbfc75150b268c
GueSosLim <guesoslim@gmail.com>
2016-06-18 23:00:34 -0500

    :/

---
+++ Wanlla/Wanlla/Areas/Admin/Controllers/ProductoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wanlla.Areas.Admin.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Admin/Producto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
