b9143355d7a1ab3a21f3eda864bbe88a15f2219d
GueSosLim <guesoslim@gmail.com>
2016-06-19 21:05:00 -0500

    receta

---
+++ Wanlla/Wanlla/Areas/Admin/Controllers/RecetaController.cs
@@ -0,0 +1,21 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Wanlla.Filters;
+
+namespace Wanlla.Areas.Admin.Controllers
+{
+    //[Autenticado]
+    public class RecetaController : Controller
+    {
+        private receta receta = new receta();
+        // GET: Admin/Receta
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
